a4fca52bae88f3ef9cfceecac9dcb4a4fd3e4729
Jefersonalves <ferreira.jefersonn@gmail.com>
2023-09-24 16:14:50 -0300

    transforms data from the opening layer to the middle layer

---
+++ 1_opening_to_middle.py
@@ -0,0 +1,110 @@
+import os
+
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder.config(
+        "spark.hadoop.fs.s3a.access.key", os.environ.get("AWS_ACCESS_KEY_ID")
+    )
+    .config("spark.hadoop.fs.s3a.secret.key", os.environ.get("AWS_SECRET_ACCESS_KEY"))
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    .config("spark.hadoop.fs.s3a.endpoint", "s3.sa-east-1.amazonaws.com")
+    .config(
+        "spark.hadoop.fs.s3a.aws.credentials.provider",
+        "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
+    )
+    .config("spark.jars.packages", "com.amazonaws:aws-java-sdk:1.12.533")
+    .config("spark.jars.packages", "org.apache.hadoop:hadoop-common:3.2.2")
+    .config("spark.jars.packages", "org.apache.hadoop:hadoop-client:3.2.2")
+    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.2")
+    .appName("app")
+    .getOrCreate()
+)
+
+SOURCE_TABLE = "s3a://chess-data-lake-opening/games-app/"
+df = spark.read.option("mode", "PERMISSIVE").json(SOURCE_TABLE)
+
+# df.printSchema()
+# root
+#  |-- black: string (nullable = true)
+#  |-- black_elo: long (nullable = true)
+#  |-- black_rating_diff: long (nullable = true)
+#  |-- eco: string (nullable = true)
+#  |-- event: string (nullable = true)
+#  |-- moves: string (nullable = true)
+#  |-- opening: string (nullable = true)
+#  |-- result: string (nullable = true)
+#  |-- site: string (nullable = true)
+#  |-- termination: string (nullable = true)
+#  |-- time_control: string (nullable = true)
+#  |-- utc_date: string (nullable = true)
+#  |-- utc_time: string (nullable = true)
+#  |-- white: string (nullable = true)
+#  |-- white_elo: long (nullable = true)
+#  |-- white_rating_diff: long (nullable = true)
+#  |-- extracted_at: date (nullable = true)
+
+df = (
+    df.withColumn("white_elo", F.col("white_elo").cast("int"))
+    .withColumn("black_elo", F.col("black_elo").cast("int"))
+    .withColumn("white_rating_diff", F.col("white_rating_diff").cast("int"))
+    .withColumn("black_rating_diff", F.col("black_rating_diff").cast("int"))
+)
+
+# Transform moves string like "1. e4 c5" to an array like ["e4", "c5"]
+df = df.withColumn("moves", F.expr(r"regexp_extract_all(moves, '[A-Za-z]+[0-9]+', 0)"))
+
+
+df = (
+    df.withColumn(
+        "utc_datetime", F.concat(F.col("utc_date"), F.lit(" "), F.col("utc_time"))
+    )
+    .withColumn(
+        "utc_datetime", F.to_timestamp(F.col("utc_datetime"), "yyyy.MM.dd HH:mm:ss")
+    )
+    .withColumn(
+        "utc_month", F.to_date(F.date_format(F.col("utc_datetime"), "yyyy-MM-01"))
+    )
+    .drop("utc_date")
+    .drop("utc_time")
+)
+
+df = df.withColumn(
+    "result",
+    F.when(F.col("result") == "1-0", "white")
+    .when(F.col("result") == "0-1", "black")
+    .otherwise(F.lit("draw")),
+)
+
+# Extract the time format of the game, the values can be bullet, blitz or classical
+df = df.withColumn("time_format", F.lower(F.split("event", r" ")[1]))
+
+df = df.withColumn(
+        "utc_month", F.to_date(F.date_format(F.col("utc_datetime"), "yyyy-MM-01"))
+    )
+
+# df.printSchema()
+# root
+#  |-- black: string (nullable = true)
+#  |-- black_elo: integer (nullable = true)
+#  |-- black_rating_diff: integer (nullable = true)
+#  |-- eco: string (nullable = true)
+#  |-- event: string (nullable = true)
+#  |-- moves: array (nullable = true)
+#  |    |-- element: string (containsNull = true)
+#  |-- opening: string (nullable = true)
+#  |-- result: string (nullable = false)
+#  |-- site: string (nullable = true)
+#  |-- termination: string (nullable = true)
+#  |-- time_control: string (nullable = true)
+#  |-- white: string (nullable = true)
+#  |-- white_elo: integer (nullable = true)
+#  |-- white_rating_diff: integer (nullable = true)
+#  |-- extracted_at: date (nullable = true)
+#  |-- utc_datetime: timestamp (nullable = true)
+#  |-- time_format: string (nullable = true)
+#  |-- utc_month: date (nullable = true)
+
+DESTINATION_TABLE = "s3a://chess-data-lake-middle/games-app/"
+df.write.partitionBy("utc_month").mode("overwrite").parquet(DESTINATION_TABLE)
